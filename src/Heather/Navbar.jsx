05c1e33866ad2dfc480933ada4eb2eeb3a3ddff5
Heather <120216007+hbryan47@users.noreply.github.com>
2023-07-15 12:47:30 -0500

    added jsx files and fixed app.js

---
+++ src/Heather/Navbar.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+
+// const NavigationBar = () => {
+// 	return (
+// 		<nav>
+// 			<ul>
+// 				<li>
+// 					<a href="/">Home</a>
+// 				</li>
+// 				<li>
+// 					<a href="/about">About</a>
+// 				</li>
+// 				<li>
+// 					<a href="/contact">Contact</a>
+// 				</li>
+// 				{/* Add more navigation links as needed */}
+// 			</ul>
+// 		</nav>
+// 	);
+// };
+
+// export default NavigationBar;
