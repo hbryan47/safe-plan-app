07733d23e4aafff7f3cda56d3f7064a35e74b734
Heather <120216007+hbryan47@users.noreply.github.com>
2023-07-16 21:00:38 -0500

    corrected img error

--- src/Heather/Navbar.jsx
+++ src/Heather/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../assets/CircularNavbar.css";
-{
+import "../img/RoseLicensedAdobeStock_479715727.jpeg";
+// {
 	/* <head>
 <script
   type="module"
@@ -11,7 +12,7 @@ import "../assets/CircularNavbar.css";
   src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
 ></script>
 </head> */
-}
+// }
 function CircularNavbar() {
 	const toggleNavbar = () => {
 		const navbar = document.querySelector(".Navbar");
@@ -53,58 +54,4 @@ function CircularNavbar() {
 	);
 }
 export default CircularNavbar;
-// import React from "react";
-// import "./assets/CircularNavbar.css";
 
-// function CircularNavbar() {
-// 	const toggleNavbar = () => {
-// 		const navbar = document.querySelector(".Navbar");
-// 		navbar.classList.toggle("active");
-// 	};
-// 	return (
-// 		<div className="Navbar">
-// 			<p className="toggle" onClick={toggleNavbar}></p>
-// 			<li style={{ "--i": 0 }}>
-// 				<a href="#">
-// 					<ion-icon name="home-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 1 }}>
-// 				<a href="#">
-// 					<ion-icon name="alert-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 2 }}>
-// 				<a href="#">
-// 					<ion-icon name="information-circle-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 3 }}>
-// 				<a href="#">
-// 					<ion-icon name="chatbubbles-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 4 }}>
-// 				<a href="#">
-// 					<ion-icon name="book-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 5 }}>
-// 				<a href="#">
-// 					<ion-icon name="person-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 6 }}>
-// 				<a href="#">
-// 					<ion-icon name="reader-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 			<li style={{ "--i": 7 }}>
-// 				<a href="#">
-// 					<ion-icon name="call-outline"></ion-icon>
-// 				</a>
-// 			</li>
-// 		</div>
-// 	);
-// }
-// export default CircularNavbar;
